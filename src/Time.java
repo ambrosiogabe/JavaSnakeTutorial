e19335c28445def4c8548e4d2fccfba5c0d0e2ea
Gabe Ambrosio <ambrosiogabe@gmail.com>
2020-07-22 10:36:15 -0500

    Remove time class

--- src/Time.java
+++
@@ -1,9 +0,0 @@
-import java.time.Duration;
-import java.time.Instant;
-
-public class Time {
-    public static double timeStarted = System.nanoTime();
-    public static Instant instantStarted = Instant.now();
-
-    public static double getTime() { return Duration.between(instantStarted, Instant.now()).toNanos() * 10E-9; }
-}
